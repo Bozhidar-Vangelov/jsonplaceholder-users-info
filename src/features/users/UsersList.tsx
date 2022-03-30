13547012671acec8cd8b60b53fb0d8b4ae866328
Bozhidar <bozhidar.vangelov@gmail.com>
2022-03-30 17:32:30 +0300

    Update home page style

--- src/features/users/UsersList.tsx
+++ src/features/users/UsersList.tsx
@@ -1,13 +1,15 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { List, Empty } from 'antd';
+import { List, Empty, Card, Space, Button } from 'antd';
 
 import { fetchUsers, usersSelector } from './usersListSlice';
 import UserCard from './UserCard';
+import UserInfo from '../userInfo/UserInfo';
 
 const UsersList = () => {
   const dispatch = useDispatch();
-  const { error, hasFetched, allUsersInfo } = useSelector(usersSelector);
+  const { error, hasFetched, allUsersInfo, loading } =
+    useSelector(usersSelector);
 
   useEffect(() => {
     // do not refetch data, as the API is not saving it
@@ -16,20 +18,45 @@ const UsersList = () => {
     }
 
     dispatch(fetchUsers());
-  }, [dispatch]);
+  }, [allUsersInfo, dispatch]);
 
   if (error) {
     return <Empty description='Failed to load data' />;
   }
 
+  // if (loading) {
+  //   return <div>LOADING</div>;
+  // }
+
   return (
-    <List
-      dataSource={allUsersInfo}
-      loading={!hasFetched}
-      className='user-list'
-      renderItem={(user) => <UserCard userInfo={user} />}
-    />
+    <section className='user-cards-container'>
+      {allUsersInfo.map((userInfo) => (
+        <Card
+          key={userInfo.id}
+          cover={
+            <img
+              alt={userInfo.name}
+              src={`https://joeschmoe.io/api/v1/${userInfo.id}`}
+              className='user-card-img'
+            />
+          }
+          className='user-card'
+        >
+          <Card.Meta title={userInfo.name} description={userInfo.username} />
+          <Button className='user-card-btn'>See user's info</Button>
+        </Card>
+      ))}
+    </section>
   );
+
+  // return (
+  //   <List
+  //     dataSource={allUsersInfo}
+  //     loading={!hasFetched}
+  //     className='user-list'
+  //     renderItem={(user) => <UserCard userInfo={user} />}
+  //   />
+  // );
 };
 
 export default UsersList;
